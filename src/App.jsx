2f5b39e0714da55536e6b7e35c497a4ddb09e4fd
galm85 <galm85@gmail.com>
2022-01-31 13:34:56 +0200

    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,69 @@ import {Home,GamesCategories,ProductPage,Signin, Register, Cart, Checkout, Hardw
 import {Dashboard} from './admin';
 import {ToastContainer,toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import * as React from 'react';
+
+
+const RenderBackgraund = ()=>{
+  const shapes = ['tri','x','circle','square'];
+  const images =[];
+
+  for(let i = 1;i<10;i++){
+    let randShape =  Math.floor(Math.random() * 4);
+    let randomTop = ( i *10 );
+    // let randomLeft = ( i *23 );
+    // let randomTop =   (Math.floor(Math.random() *i *100));
+    let randomLeft =   (Math.floor(Math.random() * 100));
+    let shape = shapes[randShape];
+    images.push({shape,randomTop,randomLeft});
+  }
+    console.log(images);
+    return (
+      <div style={{width:'100%',height:'100vh',position:'fixed',zIndex:'-2',opacity:'0.3'}}>
+          {images.map((item,index)=>(
+            <img key={index} src={`/images/${item.shape}.png`} style={{position:'absolute',top:`${item.randomTop}%`,left:`${item.randomLeft}%`,width:'500px'}}/>
+          ))}
+      </div>
+    )
+            
+  
+  
+}
+
 
 function App() {
 
   
-
+React.useEffect(()=>{
+  RenderBackgraund();
+},[])
 
   return (
    <div className="app">
      <Appbar/>
+     
      <ToastContainer  />
      <div className="main">
+
+      <div style={{position:'fixed',zIndex:'-2',opacity:'0.1'}}>
+        <img src="/images/background.png" alt="background image" style={{width:'100%',height:'100%'}} />
+      </div>
+     
+
+       {/* <div id="background" style={{position:'fixed',width:'100%',height:'100%',zIndex:'-2'}}>
+
+     <img src="/images/square.png" style={{position:'absolute',top:'56%',zIndex:'-5',left:'6%'}} />
+     <img src="/images/x.png" style={{position:'absolute',top:'19%',zIndex:'-6',left:'53%'}} />
+     <img src="/images/tri.png" style={{position:'absolute',top:'41%',zIndex:'-7',left:'13%'}} />
+     <img src="/images/circle.png" style={{position:'absolute',top:'99%',zIndex:'-8',left:'26%'}} />
+     <img src="/images/tri.png" style={{position:'absolute',top:'89%',zIndex:'-9',left:'69%'}} />
+     <img src="/images/circle.png" style={{position:'absolute',top:'90%',zIndex:'-10',left:'90%'}} />
+     <img src="/images/x.png" style={{position:'absolute',top:'57%',zIndex:'-11',left:'90%'}} />
+
+
+
+       </div> */}
+     
        <Routes>
          <Route path="/" element={<Home/>} />
          <Route path="/games" element={<GamesCategories/>} />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,12 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import { Typography,Container,Button,Grid, Divider} from '@mui/material';
 import {makeStyles} from '@mui/styles';
 import {useDispatch,useSelector} from 'react-redux';
 import { getBestSells,getComingSoon,getNewGames} from '../redux/actions/productsAction';
-import ProductCard from '../components/productCard'
+import ProductCard from '../components/productCard';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import {url} from '../config';
 
 const useStyles = makeStyles(theme=>({
     
@@ -11,6 +14,7 @@ const useStyles = makeStyles(theme=>({
     container:{
         textAlign:'center',
         margin:"50px auto",
+       
         
     },
     bannerBtn:{
@@ -40,18 +44,30 @@ const Home = () => {
     const bestSells = useSelector(state => state.products.bestSells);
     const newGames = useSelector(state => state.products.newGames);
     const comingSoon = useSelector(state => state.products.comingSoon);
+    const navigate = useNavigate();
+    const [promoteProduct,setPromoteProduct] = useState(null);
+
+    const getPromotePeoduct = async ()=>{
+        const res = await axios.get(`${url}/products/product-by-id/61e02241acceb5000477e7ac`);
+        setPromoteProduct(res.data);
+    }
 
     useEffect(()=>{
         dispatch(getBestSells());
         dispatch(getNewGames());
         dispatch(getComingSoon());
+        getPromotePeoduct();
+       
     },[])
+
+
+    
 
     return ( 
         <div className={classes.page}>
 
-           
-
+            
+          
             <div className={classes.banner}>
                 <img className={classes.bannerImage} src="./images/banner.png"  alt="games banner" />
             </div>
@@ -66,9 +82,18 @@ const Home = () => {
                 </Grid>   
             </Container>
             
-            <Divider/>
+            {promoteProduct &&
+                <div style={{position:'relative'}}>
+                    <img src="/images/unchartedBanner.png" width='100%' alt="" />
+                    <Button variant="contained" style={{position:'absolute',top:'70%',right:'10%'}} onClick={()=>navigate(`/games/uncharted-collection`,{state:promoteProduct})}>Order Now</Button>
+                </div>
+                }
             
             <Container className={classes.container}>
+                
+                
+
+
                 <Typography variant='h3' align='center'>New Games</Typography>
                 <Grid container className={classes.gamesRow}>
                 {newGames && newGames.map(row=>(
@@ -77,7 +102,12 @@ const Home = () => {
                 </Grid>   
             </Container>
             
-            <Divider/>
+            {promoteProduct &&
+                <div style={{position:'relative'}}>
+                    <img src="/images/ps5banner.png" width='100%' alt="ps5 banner" />
+                    
+                </div>
+                }
             
             <Container  className={classes.container}>
                 <Typography variant='h3' align='center'>Pre Order</Typography>
@@ -88,6 +118,7 @@ const Home = () => {
                 </Grid>   
             </Container>
 
+            
             
         </div>
      );
